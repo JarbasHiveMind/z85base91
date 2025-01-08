cfb7702eff31322e57dbed7da202bac548f525d4
miro <jarbasai@mailfence.com>
2025-01-08 16:19:47 +0000

    pkg

---
+++ setup.py
@@ -0,0 +1,50 @@
+import os
+from setuptools import setup
+
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+
+
+def get_version():
+    """ Find the version of the package"""
+    version_file = os.path.join(BASEDIR, 'zbase', 'version.py')
+    major, minor, build, alpha = (None, None, None, None)
+    with open(version_file) as f:
+        for line in f:
+            if 'VERSION_MAJOR' in line:
+                major = line.split('=')[1].strip()
+            elif 'VERSION_MINOR' in line:
+                minor = line.split('=')[1].strip()
+            elif 'VERSION_BUILD' in line:
+                build = line.split('=')[1].strip()
+            elif 'VERSION_ALPHA' in line:
+                alpha = line.split('=')[1].strip()
+
+            if ((major and minor and build and alpha) or
+                    '# END_VERSION_BLOCK' in line):
+                break
+    version = f"{major}.{minor}.{build}"
+    if int(alpha):
+        version += f"a{alpha}"
+    return version
+
+
+def required(requirements_file):
+    """ Read requirements file and remove comments and empty lines. """
+    with open(os.path.join(BASEDIR, requirements_file), 'r') as f:
+        requirements = f.read().splitlines()
+        return [pkg for pkg in requirements
+                if pkg.strip() and not pkg.startswith("#")]
+
+
+
+setup(
+    name='zbase',
+    version=get_version(),
+    packages=['zbase'],
+    url='https://github.com/JarbasHiveMind/zbase',
+    license='Apache-2.0',
+    author='jarbasAi',
+    include_package_data=True,
+    author_email='jarbasai@mailfence.com',
+    description='base91, z85b and z85p encodings'
+)
